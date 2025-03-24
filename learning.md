4e4af487e5cae8986bc07e70b84452e401da3ded
kaushik <prajapatikaushik6392@gmail.com>
2025-03-24 18:53:30 +0530

    how to handlling errors and make file for error  handling on utils folder

---
+++ learning.md
@@ -0,0 +1,11 @@
+cookies
+cors
+
+app.js folder makw server and nakew another folder in which use app.listen without importing app hoe is posible
+
+whatbis url hit
+what is middleware
+what is flag (middleware next \ iddleware next)
+learn about error code
+nodejs API errors (node.js error class)
+server status code
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,42 @@
+// using promises (resolve and reject(catch))
+
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>  {
+        Promise
+        .resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+/*
+using try and catch
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => { () => {}}
+// const asyncHandler = (func) =>  () => {}
+// const asyncHandler = (func) =>  async () => {}
+
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+
+*/
